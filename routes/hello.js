636a1b6f372afb8b0ef4c5bd7178e29abf8b76f8
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-10-14 22:59:44 +0900

    fix typo

--- routes/hello.js
+++ routes/hello.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  var lastName = '会員確認のため、お名前をご記入ください';
-  if(req.sesssion.useName != undifined){
+  var lastName = '会員確認のため、お名前を送信してください';
+  if(req.session.userName != undefined){
     lastName = "直近の確認者:" +  req.session.userName;
   }
   var data = {
-    title1: '会員登録情報の確認',
+    title: '会員登録情報の確認',
     content: lastName
   }
   res.render('hello', data);
 });
 
 router.post('/confirm', (req, res, next) => {
-  var name = req.body['userName'];
+  var lastName = req.body['userName'];
+  req.session.userName = lastName
   var data = {
     title: '登録情報の確認完了！',
     content: "直近の確認者:" +  req.session.userName
